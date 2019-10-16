1a7868581355f8592c4b101f2c498d6c00cd628c
amitil <biloholovskyi>
2019-10-16 23:05:59 +0300

    gulp.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,29 @@
-var gulp = require('gulp');
-var browserSync = require('browser-sync').create();
-var sass = require('gulp-sass');
-var imagemin = require('gulp-imagemin');
-var minCSS = require('gulp-clean-css');
-var minJS = require('gulp-minify');
-var clean = require('gulp-clean');
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass');
+const minCSS = require('gulp-clean-css');
 const babel = require('gulp-babel');
 
 gulp.task('default', function() {
     browserSync.init({
-      proxy: "http://localhost:8888/ertc_new_wp/app",
-      // port: 8888
+      proxy: "http://localhost:8888/ertc_new/app",
     });
-    gulp.watch("app/sass/**/*.scss", function () {
-        return gulp.src("app/sass/**/*.scss")
+    gulp.watch("app/wp-content/themes/erts/sass/**/*.scss", function () {
+        return gulp.src("app/wp-content/themes/erts/sass/**/*.scss")
           .pipe(sass())
           .pipe(minCSS())
-          .pipe(gulp.dest("app/css"))
+          .pipe(gulp.dest("app/wp-content/themes/erts/css"))
           .pipe(browserSync.stream());
     });
-    gulp.watch("app/js/**/*.js", function () {
-      return gulp.src("app/js/index.js")
+    gulp.watch("app/wp-content/themes/erts/js/**/*.js", function () {
+      return gulp.src("app/wp-content/themes/erts/js/index.js")
         .pipe(babel({
           presets: ['@babel/env']
         }))
-        .pipe(gulp.dest("app/buildjs"))
+        .pipe(gulp.dest("app/wp-content/themes/erts/buildjs"))
         .pipe(browserSync.stream());
     });
     gulp.watch("app/**/*.php").on('change', browserSync.reload);
     gulp.watch("app/**/*.html").on('change', browserSync.reload);
-    gulp.watch("app/js/*.js").on('change', browserSync.reload);
+    gulp.watch("app/wp-content/themes/erts/js/*.js").on('change', browserSync.reload);
 });
